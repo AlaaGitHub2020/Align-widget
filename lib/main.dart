11d6f85c03ae698442215ea587d789054311ff92
Alaa Mashkouk <alaamashkouk2@gmail.com>
2020-05-28 16:09:18 +0530

    Align Widget example

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:aignwidgetexample/my_home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Align Widget Example',
+    home: MyHome(),
+  ));
+}
---
+++ lib/my_home.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyHome extends StatefulWidget {
+  @override
+  _MyHomeState createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.redAccent,
+        title: Text(
+          'Align Widget Example',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 25.0,
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Container(
+        child: Align(
+          alignment: Alignment.center,
+          child: MyPurpleBox(),
+        ),
+      ),
+    );
+  }
+
+  Widget MyYellowBox() {
+    return Container(
+      color: Colors.yellow,
+      width: 25.0,
+      height: 25.0,
+      child: Center(
+        child: Text(
+          'Yellow Box',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 25.0,
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget MyRedBox() {
+    return Container(
+      color: Colors.red,
+      width: 50.0,
+      height: 50.0,
+      child: Center(
+        child: Text(
+          'Red Box',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 25.0,
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget MyBlueBox() {
+    return Container(
+      color: Colors.blue,
+      width: 100.0,
+      height: 100.0,
+      child: Center(
+        child: Text(
+          'Blue Box',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 25.0,
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget MyGreenBox() {
+    return Container(
+      color: Colors.green,
+      width: 150.0,
+      height: 150.0,
+      child: Center(
+        child: Text(
+          'Green Box',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 25.0,
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget MyPurpleBox() {
+    return Container(
+      color: Colors.purple,
+      width: 200.0,
+      height: 200.0,
+      child: Center(
+        child: Text(
+          'Purple Box',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 25.0,
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
